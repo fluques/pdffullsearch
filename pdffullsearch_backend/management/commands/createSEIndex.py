e388ad7c9848950c9ed41dda75c10d1e389e41f7
Fernando <ing.fernnado.luque@gmail.com>
2026-02-07 18:33:00 -0700

    changes on compose.yaml

--- pdffullsearch_backend/management/commands/createSEIndex.py
+++ pdffullsearch_backend/management/commands/createSEIndex.py
@@ -1,3 +1,4 @@
+import time
 from django.core.management.base import BaseCommand
 from django.contrib.auth.models import User
 from elasticsearch_dsl.connections import connections
@@ -9,6 +10,17 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         try:
+            tries = 0
+            while(tries < 5):
+                try:
+                    es = connections.get_connection()
+                    es.ping()
+                    break
+                except Exception as e:
+                    tries += 1
+                    self.stdout.write(self.style.WARNING(f"Attempt {tries}: Could not connect to Elasticsearch, retrying..."))
+                    time.sleep(5)  # Wait for 5 seconds before retrying
+
             es = connections.get_connection()                
             # Check if the index exists and create if not
             index_name = PDFFileDocument._index._name
